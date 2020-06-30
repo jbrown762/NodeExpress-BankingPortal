dfbdc47f93e1ec8817466ceb2f96c92c0d6ea007
Brown, Joseph W <Joseph.W.Brown@ssa.gov>
2020-06-30 14:32:31 -0400

    call writeJSON

--- src/app.js
+++ src/app.js
@@ -31,8 +31,7 @@ app.get('/payment', (req, res) => res.render('payment', { account: accounts.cred
 app.post('/payment', (req, res) => {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount);
-  let accountsJSON = JSON.stringify(accounts, null, 4)
-  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+  writeJSON();
   res.render('payment', { message: 'Payment Successful', account: accounts.credit });
 });
 
